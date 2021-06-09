e7d9d60826ad6bb955f2d25c36a37f905423ddfd
Phan Thai Duong <30502963+phanthaiduong22@users.noreply.github.com>
2021-06-09 23:09:07 +0700

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,37 +1,32 @@
-int partition (int arr[], int low, int high) 
-{ 
-    int pivot = arr[high];    // pivot 
-    int i = (low - 1);  // Index of smaller element 
-  
-    for (int j = low; j <= high- 1; j++) 
-    { 
-        // If current element is smaller than or 
-        // equal to pivot 
-        if (arr[j] <= pivot) 
-        { 
-            i++;    // increment index of smaller element 
-            swap(&arr[i], &arr[j]); 
-        } 
-    } 
-    swap(&arr[i + 1], &arr[high]); 
-    return (i + 1); 
-} 
-  
-/* The main function that implements QuickSort 
- arr[] --> Array to be sorted, 
-  low  --> Starting index, 
-  high  --> Ending index */
-void quickSort(int arr[], int low, int high) 
-{ 
-    if (low < high) 
-    { 
-        /* pi is partitioning index, arr[p] is now 
-           at right place */
-        int pi = partition(arr, low, high); 
-  
-        // Separately sort elements before 
-        // partition and after partition 
-        quickSort(arr, low, pi - 1); 
-        quickSort(arr, pi + 1, high); 
-    } 
-} 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int partition(vector<int>&a, int l, int r){
+    int pivot = a[r];
+
+    int i = l - 1;
+    for(int j = l; j<= r- 1; ++j){
+        if(a[j] < pivot){
+            i++;
+            swap(a[i], a[j]);
+        }
+    }
+    swap(a[i+1], a[r]);
+    return i + 1;
+}
+
+void quicksort(vector<int>&a, int l, int r){
+    if(l<r){
+        int pivot = partition(a, l, r);
+        quicksort(a, l, pivot - 1);
+        quicksort(a, pivot + 1, r);
+    }
+}
+
+int main(){
+    vector<int>a = {4, 3, 5, 7, 2, 1, 3, 7};
+    quicksort(a, 0, a.size() - 1);   
+    for(int i : a)
+        cout<<i<<" ";
+}
